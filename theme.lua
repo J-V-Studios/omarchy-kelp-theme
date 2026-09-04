38d1c583f66506a7bcd35575e5f7dacde22991cd
Lil Grip <josiahedev@gmail.com>
2026-09-04 09:12:25 -0700

    Replace hyprland.lua with hyprland.conf and remove untracked files

--- theme.lua
+++
@@ -1,3 +0,0 @@
--- Append `&` inside the commands to execute them asynchronously in the background
-os.execute("gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &")
-os.execute("papirus-folders -C teal -t Papirus-Dark -u &")
